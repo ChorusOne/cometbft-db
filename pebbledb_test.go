47aadcf54802d6fa4ec31dbb1f8256b9394887fb
Sergey Mishin <sergey@chorus.one>
2023-10-13 12:51:40 +0200

    Add pebbledb backend test

---
+++ pebbledb_test.go
@@ -0,0 +1,26 @@
+//go:build pebbledb
+// +build pebbledb
+
+package db
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPebbleDBBackend(t *testing.T) {
+	name := fmt.Sprintf("test_%x", randStr(12))
+	dir := os.TempDir()
+	db, err := NewDB(name, PebbleDBBackend, dir)
+	require.NoError(t, err)
+	defer cleanupDBDir(dir, name)
+
+	_, ok := db.(*PebbleDB)
+	assert.True(t, ok)
+}
+
+// TODO: Add tests for pebbledb
